18faa3421e0b9ad0f0c4241caa5f65eab138d650
Skykey <zcxzxlc@163.com>
2021-09-24 21:09:58 +0800

    初步定义API接口，添加单元测试

---
+++ C/dianajson.h
@@ -0,0 +1,39 @@
+#ifndef DIANAJSON_H
+#define DIANAJSON_H
+
+/* Json 7种数据类型 */
+/* 使用项目简写作为标识符的前缀（C无CPP命名空间功能） */
+typedef enum
+{
+    DIANA_NULL,
+    DIANA_FALSE,
+    DIANA_TRUE,
+    DIANA_NUMBER,
+    DIANA_STRING,
+    DIANA_ARRAY,
+    DIANA_OBJECT
+} diana_type; // JSON数据类型
+
+/* JSON数据结构 */
+/* 树形结构 */
+typedef struct
+{
+    diana_type type;
+} diana_value; // 树形结构的每个节点使用diana_value表示，也称它为一个值（JSON Value）。
+
+/* JSON解析返回值 */
+enum
+{
+    DIANA_PARSE_OK = 0,
+    DIANA_PARSE_EXPECT_VALUE,     // JSON只含有空白
+    DIANA_PARSE_INVALID_VALUE,    // 属于非法字面值
+    DIANA_PARSE_ROOT_NOT_SINGULAR // 若一个值之后，在空白之后还有其他字符
+};
+
+/* JSON解析 */
+int lept_parse(diana_value *v, const char *json); // 解析JSON，根节点指针v是由使用方负责分配
+
+/* 获取访问结果函数，获取其类型 */
+diana_type diana_get_type(const diana_value *v);
+
+#endif /* DIANAJSON */
