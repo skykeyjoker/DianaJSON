70d6946660ab6fd9caf588ceeb32796ae7f0a8e8
Skykey <zcxzxlc@163.com>
2021-10-04 11:42:34 +0800

    更新字符串unicode支持

--- C/dianajson.c
+++ C/dianajson.c
@@ -152,8 +152,63 @@ static int diana_parse_number(diana_context *c, diana_value *v)
 }
 
 /* 解析字符串 */
+#define STRING_ERROR(ret) \
+    do                    \
+    {                     \
+        c->top = head;    \
+        return ret;       \
+    } while (0)
+
+/* 读取4位16进制数字 */
+static const char *diana_parse_hex4(const char *p, unsigned *u)
+{
+    int i;
+    *u = 0;
+    for (i = 0; i < 4; ++i)
+    {
+        char ch = *p++;
+        *u <<= 4;
+        if (ch >= '0' && ch <= '9')
+            *u |= ch - '0';
+        else if (ch >= 'A' && ch <= 'F')
+            *u |= ch - ('A' - 10);
+        else if (ch >= 'a' && ch <= 'f')
+            *u |= ch - ('a' - 10);
+        else
+            return NULL;
+    }
+    return p;
+}
+
+/* 转码为utf8 */
+static void diana_encode_utf8(diana_context *c, unsigned u)
+{
+    if (u <= 0x7F)
+        PUTC(c, u & 0xFF);
+    else if (u <= 0x7FF)
+    {
+        PUTC(c, 0xC0 | ((u >> 6) & 0xFF));
+        PUTC(c, 0x80 | (u & 0x3F));
+    }
+    else if (u <= 0xFFFF)
+    {
+        PUTC(c, 0xE0 | ((u >> 12) & 0xFF));
+        PUTC(c, 0x80 | ((u >> 6) & 0x3F));
+        PUTC(c, 0x80 | (u & 0x3F));
+    }
+    else
+    {
+        assert(u <= 0x10FFFF);
+        PUTC(c, 0xF0 | ((u >> 18) & 0xFF));
+        PUTC(c, 0x80 | ((u >> 12) & 0x3F));
+        PUTC(c, 0x80 | ((u >> 6) & 0x3F));
+        PUTC(c, 0x80 | (u & 0x3F));
+    }
+}
+
 static int diana_parse_string(diana_context *c, diana_value *v)
 {
+    unsigned u, u2;
     size_t head = c->top, len;
     const char *p;
     EXPECT(c, '\"');
@@ -195,20 +250,33 @@ static int diana_parse_string(diana_context *c, diana_value *v)
             case 't':
                 PUTC(c, '\t');
                 break;
+            case 'u':
+                if (!(p = diana_parse_hex4(p, &u)))
+                    STRING_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX);
+                /* surrogate handling */
+                if (u >= 0xD800 && u <= 0xDBFF)
+                { /* surrogate pair */
+                    if (*p++ != '\\')
+                        STRING_ERROR(DIANA_PARSE_INVALID_UNICODE_SURROGATE);
+                    if (*p++ != 'u')
+                        STRING_ERROR(DIANA_PARSE_INVALID_UNICODE_SURROGATE);
+                    if (!(p = diana_parse_hex4(p, &u2)))
+                        STRING_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX);
+                    if (u2 < 0xDC00 || u2 > 0xDFFF)
+                        STRING_ERROR(DIANA_PARSE_INVALID_UNICODE_SURROGATE);
+                    u = (((u - 0xD800) << 10) | (u2 - 0xDC00)) + 0x10000;
+                }
+                diana_encode_utf8(c, u);
+                break;
             default:
-                c->top = head;
-                return DIANA_PARSE_INVALID_STRING_ESCAPE;
+                STRING_ERROR(DIANA_PARSE_INVALID_STRING_ESCAPE);
             }
             break;
         case '\0':
-            c->top = head;
-            return DIANA_PARSE_MISS_QUOTATION_MARK;
+            STRING_ERROR(DIANA_PARSE_MISS_QUOTATION_MARK);
         default:
             if ((unsigned char)ch < 0x20)
-            {
-                c->top = head;
-                return DIANA_PARSE_INVALID_STRING_CHAR;
-            }
+                STRING_ERROR(DIANA_PARSE_INVALID_STRING_CHAR);
             PUTC(c, ch);
         }
     }
--- C/test.c
+++ C/test.c
@@ -118,6 +118,12 @@ static void test_parse_string()
     TEST_STRING("Hello", "\"Hello\"");
     TEST_STRING("Hello\nWorld", "\"Hello\\nWorld\"");
     TEST_STRING("\" \\ / \b \f \n \r \t", "\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\"");
+    TEST_STRING("Hello\0World", "\"Hello\\u0000World\"");
+    TEST_STRING("\x24", "\"\\u0024\"");                    /* Dollar sign U+0024 */
+    TEST_STRING("\xC2\xA2", "\"\\u00A2\"");                /* Cents sign U+00A2 */
+    TEST_STRING("\xE2\x82\xAC", "\"\\u20AC\"");            /* Euro sign U+20AC */
+    TEST_STRING("\xF0\x9D\x84\x9E", "\"\\uD834\\uDD1E\""); /* G clef sign U+1D11E */
+    TEST_STRING("\xF0\x9D\x84\x9E", "\"\\ud834\\udd1e\""); /* G clef sign U+1D11E */
 }
 
 #define TEST_ERROR(error, json)                        \
@@ -188,6 +194,50 @@ static void test_parse_invalid_string_char()
     TEST_ERROR(DIANA_PARSE_INVALID_STRING_CHAR, "\"\x1F\"");
 }
 
+static void test_parse_invalid_unicode_hex()
+{
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\u\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\u0\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\u01\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\u012\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\u/000\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\uG000\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\u0/00\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\u0G00\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\u00/0\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\u00G0\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\u000/\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\u000G\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_HEX, "\"\\u 123\"");
+}
+
+static void test_parse_invalid_unicode_surrogate()
+{
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_SURROGATE, "\"\\uD800\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_SURROGATE, "\"\\uDBFF\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_SURROGATE, "\"\\uD800\\\\\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_SURROGATE, "\"\\uD800\\uDBFF\"");
+    TEST_ERROR(DIANA_PARSE_INVALID_UNICODE_SURROGATE, "\"\\uD800\\uE000\"");
+}
+
+static void test_parse()
+{
+    test_parse_null();
+    test_parse_true();
+    test_parse_false();
+    test_parse_number();
+    test_parse_string();
+    test_parse_expect_value();
+    test_parse_invalid_value();
+    test_parse_root_not_singular();
+    test_parse_number_too_big();
+    test_parse_missing_quotation_mark();
+    test_parse_invalid_string_escape();
+    test_parse_invalid_string_char();
+    test_parse_invalid_unicode_hex();
+    test_parse_invalid_unicode_surrogate();
+}
+
 static void test_access_null()
 {
     diana_value v;
@@ -231,21 +281,8 @@ static void test_access_string()
     diana_free(&v);
 }
 
-static void test_parse()
+static void test_access()
 {
-    test_parse_null();
-    test_parse_true();
-    test_parse_false();
-    test_parse_number();
-    test_parse_string();
-    test_parse_expect_value();
-    test_parse_invalid_value();
-    test_parse_root_not_singular();
-    test_parse_number_too_big();
-    test_parse_missing_quotation_mark();
-    test_parse_invalid_string_escape();
-    test_parse_invalid_string_char();
-
     test_access_null();
     test_access_boolean();
     test_access_number();
@@ -255,6 +292,7 @@ static void test_parse()
 int main()
 {
     test_parse();
+    test_access();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
     return main_ret;
 }
