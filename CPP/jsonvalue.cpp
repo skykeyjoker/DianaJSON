4b6da2324d9baedb30fa58ee50d1194225951a49
Skykey <zcxzxlc@163.com>
2021-10-22 11:28:08 +0800

    更换代码样式

--- CPP/jsonvalue.cpp
+++ CPP/jsonvalue.cpp
@@ -2,90 +2,91 @@
 //#include "jsonerror.h"
 
 namespace DianaJSON {
-JsonValueType JsonValue::getType() const noexcept {
-  if (std::holds_alternative<std::nullptr_t>(_val))
-    return JsonValueType::Null;
-  else if (std::holds_alternative<bool>(_val))
-    return JsonValueType::Bool;
-  else if (std::holds_alternative<double>(_val))
-    return JsonValueType::Number;
-  else if (std::holds_alternative<std::string>(_val))
-    return JsonValueType::String;
-  else if (std::holds_alternative<Json::_array>(_val))
-    return JsonValueType::Array;
-  else if (std::holds_alternative<Json::_object>(_val))
-    return JsonValueType::Object;
-}
+	JsonValueType JsonValue::getType() const noexcept {
+		if (std::holds_alternative<std::nullptr_t>(_val))
+			return JsonValueType::Null;
+		else if (std::holds_alternative<bool>(_val))
+			return JsonValueType::Bool;
+		else if (std::holds_alternative<double>(_val))
+			return JsonValueType::Number;
+		else if (std::holds_alternative<std::string>(_val))
+			return JsonValueType::String;
+		else if (std::holds_alternative<Json::_array>(_val))
+			return JsonValueType::Array;
+		else if (std::holds_alternative<Json::_object>(_val))
+			return JsonValueType::Object;
+	}
 
-bool JsonValue::isNull() const {
-  return std::holds_alternative<std::nullptr_t>(_val);
-}
+	bool JsonValue::isNull() const {
+		return std::holds_alternative<std::nullptr_t>(_val);
+	}
 
-bool JsonValue::isBoolean() const { return std::holds_alternative<bool>(_val); }
+	bool JsonValue::isBoolean() const { return std::holds_alternative<bool>(_val); }
 
-bool JsonValue::isString() const {
-  return std::holds_alternative<std::string>(_val);
-}
+	bool JsonValue::isString() const {
+		return std::holds_alternative<std::string>(_val);
+	}
 
-bool JsonValue::isNumber() const {
-  return std::holds_alternative<double>(_val);
-}
+	bool JsonValue::isNumber() const {
+		return std::holds_alternative<double>(_val);
+	}
 
-bool JsonValue::isArray() const {
-  return std::holds_alternative<Json::_array>(_val);
-}
+	bool JsonValue::isArray() const {
+		return std::holds_alternative<Json::_array>(_val);
+	}
 
-bool JsonValue::isObject() const {
-  return std::holds_alternative<Json::_object>(_val);
-}
+	bool JsonValue::isObject() const {
+		return std::holds_alternative<Json::_object>(_val);
+	}
 
-bool JsonValue::toBool() const { return std::get<bool>(_val); }
+	bool JsonValue::toBool() const { return std::get<bool>(_val); }
 
-double JsonValue::toDouble() const { return std::get<double>(_val); }
+	double JsonValue::toDouble() const { return std::get<double>(_val); }
 
-const std::string &JsonValue::toString() const {
-  return std::get<std::string>(_val);
-}
+	const std::string &JsonValue::toString() const {
+		return std::get<std::string>(_val);
+	}
 
-const Json::_array &JsonValue::toArray() const {
-  return std::get<Json::_array>(_val);
-}
+	const Json::_array &JsonValue::toArray() const {
+		return std::get<Json::_array>(_val);
+	}
 
-const Json::_object &JsonValue::toObject() const {
-  return std::get<Json::_object>(_val);
-}
+	const Json::_object &JsonValue::toObject() const {
+		return std::get<Json::_object>(_val);
+	}
 
-size_t JsonValue::size() const {
-  if (std::holds_alternative<Json::_array>(_val))
-    return std::get<Json::_array>(_val).size();
-  else if (std::holds_alternative<Json::_object>(_val))
-    return std::get<Json::_object>(_val).size();
-  else {
-    // TODO 非数组或者对象类型报错
-  }
-}
-const Json &JsonValue::operator[](size_t index) const {
-  if (std::holds_alternative<Json::_array>(_val)) {
-    return std::get<Json::_array>(_val)[index];
-  } else {
-    // TODO 非数组类型报错
-  }
-}
+	size_t JsonValue::size() const {
+		if (std::holds_alternative<Json::_array>(_val))
+			return std::get<Json::_array>(_val).size();
+		else if (std::holds_alternative<Json::_object>(_val))
+			return std::get<Json::_object>(_val).size();
+		else {
+			// TODO 非数组或者对象类型报错
+		}
+	}
 
-Json &JsonValue::operator[](size_t index) {
-  return const_cast<Json &>(static_cast<const JsonValue &>(*this)[index]);
-}
+	const Json &JsonValue::operator[](size_t index) const {
+		if (std::holds_alternative<Json::_array>(_val)) {
+			return std::get<Json::_array>(_val)[index];
+		} else {
+			// TODO 非数组类型报错
+		}
+	}
 
-const Json &JsonValue::operator[](const std::string &key) const {
-  if (std::holds_alternative<Json::_object>(_val)) {
-    return std::get<Json::_object>(_val).at(key);
-  } else {
-    // TODO 非对象类型报错
-  }
-}
+	Json &JsonValue::operator[](size_t index) {
+		return const_cast<Json &>(static_cast<const JsonValue &>(*this)[index]);
+	}
 
-Json &JsonValue::operator[](const std::string &key) {
-  return const_cast<Json &>(static_cast<const JsonValue &>(*this)[key]);
-}
+	const Json &JsonValue::operator[](const std::string &key) const {
+		if (std::holds_alternative<Json::_object>(_val)) {
+			return std::get<Json::_object>(_val).at(key);
+		} else {
+			// TODO 非对象类型报错
+		}
+	}
 
-} // namespace DianaJSON
+	Json &JsonValue::operator[](const std::string &key) {
+		return const_cast<Json &>(static_cast<const JsonValue &>(*this)[key]);
+	}
+
+}// namespace DianaJSON
--- CPP/jsonvalue.h
+++ CPP/jsonvalue.h
@@ -7,62 +7,62 @@
 #include <variant>
 
 namespace DianaJSON {
-// 实现内部类JsonValue
-class JsonValue {
-public:
-  // 构造函数
-  explicit JsonValue(std::nullptr_t) : _val(nullptr) {}
-  explicit JsonValue(bool val) : _val(val) {}
-  explicit JsonValue(double val) : _val(val) {}
-  explicit JsonValue(const std::string &val) : _val(val) {}
-  explicit JsonValue(const Json::_array &val) : _val(val) {}
-  explicit JsonValue(const Json::_object &val) : _val(val) {}
+	// 实现内部类JsonValue
+	class JsonValue {
+	public:
+		// 构造函数
+		explicit JsonValue(std::nullptr_t) : _val(nullptr) {}
+		explicit JsonValue(bool val) : _val(val) {}
+		explicit JsonValue(double val) : _val(val) {}
+		explicit JsonValue(const std::string &val) : _val(val) {}
+		explicit JsonValue(const Json::_array &val) : _val(val) {}
+		explicit JsonValue(const Json::_object &val) : _val(val) {}
 
-public:
-  // 移动构造函数
-  explicit JsonValue(std::string &&val) : _val(std::move(val)) {}
-  explicit JsonValue(Json::_array &&val) : _val(std::move(val)) {}
-  explicit JsonValue(Json::_object &&val) : _val(std::move(val)) {}
+	public:
+		// 移动构造函数
+		explicit JsonValue(std::string &&val) : _val(std::move(val)) {}
+		explicit JsonValue(Json::_array &&val) : _val(std::move(val)) {}
+		explicit JsonValue(Json::_object &&val) : _val(std::move(val)) {}
 
-public:
-  // 析构函数
-  ~JsonValue() = default;
+	public:
+		// 析构函数
+		~JsonValue() = default;
 
-public:
-  // Array与Object类的访问接口
-  size_t size() const; // 返回数组或对象成员数
+	public:
+		// Array与Object类的访问接口
+		size_t size() const;// 返回数组或对象成员数
 
-  // 数组，支持随机访问
-  const Json &operator[](size_t index) const;
-  Json &operator[](size_t index);
+		// 数组，支持随机访问
+		const Json &operator[](size_t index) const;
+		Json &operator[](size_t index);
 
-  // 对象，O(1)遍历寻找（不支持插入）
-  const Json &operator[](const std::string &key) const;
-  Json &operator[](const std::string &key);
+		// 对象，O(1)遍历寻找（不支持插入）
+		const Json &operator[](const std::string &key) const;
+		Json &operator[](const std::string &key);
 
-public:
-  // 数据类型判断接口
-  JsonValueType getType() const noexcept;
-  bool isNull() const;
-  bool isBoolean() const;
-  bool isNumber() const;
-  bool isString() const;
-  bool isArray() const;
-  bool isObject() const;
+	public:
+		// 数据类型判断接口
+		JsonValueType getType() const noexcept;
+		bool isNull() const;
+		bool isBoolean() const;
+		bool isNumber() const;
+		bool isString() const;
+		bool isArray() const;
+		bool isObject() const;
 
-public:
-  // 数据类型转换接口
-  bool toBool() const;
-  double toDouble() const; // Number类型实际表现为double类型浮点数
-  const std::string &toString() const;
-  const Json::_array &toArray() const;
-  const Json::_object &toObject() const;
+	public:
+		// 数据类型转换接口
+		bool toBool() const;
+		double toDouble() const;// Number类型实际表现为double类型浮点数
+		const std::string &toString() const;
+		const Json::_array &toArray() const;
+		const Json::_object &toObject() const;
 
-private:
-  std::variant<std::nullptr_t, bool, double, std::string, Json::_array,
-               Json::_object>
-      _val; // 使用variant储存多元类型，节省空间
-};
-} // namespace DianaJSON
+	private:
+		std::variant<std::nullptr_t, bool, double, std::string, Json::_array,
+					 Json::_object>
+				_val;// 使用variant储存多元类型，节省空间
+	};
+}// namespace DianaJSON
 
 #endif
